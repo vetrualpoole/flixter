d0be88f1031bae4b97589a3bff30c28e6fa1c0a0
Vetrual Poole <vetrualpoole@gmail.com>
2019-11-17 01:14:48 +0000

    setting up student enrollment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def enrolled_in?(course)
+    enrolled_courses = enrollments.collect(&:course)    
+    return enrolled_courses.include?(course)
+  end
+
 end
