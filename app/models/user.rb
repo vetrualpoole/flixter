3d49f8d5c5aee1ff588e21ebb3e92bff98efb778
Vetrual Poole <vetrualpoole@gmail.com>
2019-11-17 16:45:54 +0000

    corrected user.rb on student enrollment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
+  has_many :enrolled_courses, through: :enrollments, source: :course
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   def enrolled_in?(course)
-    enrolled_courses = enrollments.collect(&:course)    
     return enrolled_courses.include?(course)
   end
 
