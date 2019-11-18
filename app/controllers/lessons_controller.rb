cc7881d0a102208acf98e2ae42c098d5fe6ca930
Vetrual Poole <vetrualpoole@gmail.com>
2019-11-18 03:05:23 +0000

    lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,28 @@
 class LessonsController < ApplicationController
-    def show
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
+  def show
+      @lesson = Lesson.show
     end
     
     private
+
+    def require_authorized_for_current_section
+      if current_section.course.user != current_user
+        render plain: 'Unauthorized', status: :Unauthorized
+        redirect_to course_path(current_course)
+      end
+     end
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video)
+  end  
 end
+
+
